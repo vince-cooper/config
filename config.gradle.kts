cbaf4e2e43f50fac2cc2fbe5f9e1ea8237a041d0
wenhui.w <wangwenhui@51Talk.com>
2024-10-23 19:14:51 +0800

    feat: 添加全局配置文件

---
+++ config.gradle.kts
@@ -0,0 +1,5 @@
+// Nexus配置
+extra["nexusUsername"] = "admin"
+extra["nexusPassword"] = "abc123456"
+extra["nexusReleaseUrl"] = "http://127.0.0.1:8081/repository/maven-releases/"
+extra["nexusSnapshotUrl"] = "http://127.0.0.1:8081/repository/maven-snapshots/"
